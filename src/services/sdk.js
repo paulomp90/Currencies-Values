f0a7956c2cfdaddb6cc1d8eb1c91aca7a441cbfa
paulo <paulo@attentive.us>
2020-03-26 23:39:09 +0000

    first commit

--- src/services/sdk.js
+++ src/services/sdk.js
@@ -2,6 +2,6 @@ import SDK from '@uphold/uphold-sdk-javascript';
 
 export const sdk = new SDK({
     baseUrl: 'http://api-sandbox.uphold.com',
-    clientId: 'd7f2fa3336751e30c1a84d97180408280d0f5bb7',
-    clientSecret: '8d787c56db4438912fe26520a885a7a0fc440160'
+    clientId: '',
+    clientSecret: ''
 });
